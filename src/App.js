647c7b900906bea42fece2cda8f0e8a746847450
sudeep s patil <viruss@sudeeps-MacBook-Air.local>
2022-05-27 15:38:22 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <p>this is paragraph</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react'
+import App from './App'
+
+render(
+    <App/>,
+    document.getElementById('root')
+)
